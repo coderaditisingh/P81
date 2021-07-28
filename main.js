90e7b966eade1be5c0ebd39b8e5ae28aec56e455
coderaditisingh <86613587+coderaditisingh@users.noreply.github.com>
2021-07-28 18:13:19 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,37 @@
+var canvas=document.getElementById("mycanvas");
+ctx= canvas.getContext("2d");
+color="purple";
+
+ctx.beginPath();
+ctx.strokeStyle= "blue";
+ctx.lineWidth =4;
+ctx.arc(200,200,40,0,2*Math.PI);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.strokeStyle="black";
+ctx.lineWidth =4;
+ctx.arc(290,200,40,0,2*Math.PI);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.strokeStyle= "red";
+ctx.lineWidth =4;
+ctx.arc(380,200,40,0,2*Math.PI);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.strokeStyle= "yellow";
+ctx.lineWidth =4;
+ctx.arc(255,245,45,0,2*Math.PI);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.strokeStyle= "green";
+ctx.lineWidth =4;
+ctx.arc(330,245,45,0,2*Math.PI);
+ctx.stroke();
+function cleararea()
+{
+ctx.clearRect(0,0,canvas.width,canvas.height);
+}
