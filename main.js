b1b5c2e4e0ffa3cdc2baae206c8b49d2c916fabe
coderaditisingh <86613587+coderaditisingh@users.noreply.github.com>
2021-07-30 19:56:34 +0530

    background image not working

--- main.js
+++ main.js
@@ -31,6 +31,13 @@ ctx.strokeStyle= "green";
 ctx.lineWidth =4;
 ctx.arc(330,245,45,0,2*Math.PI);
 ctx.stroke();
+
+ctx.beginPath();
+ctx.strokeStyle= "pink";
+ctx.lineWidth =6;
+ctx.rect(80,143,430,200);
+ctx.stroke();
+
 function cleararea()
 {
 ctx.clearRect(0,0,canvas.width,canvas.height);
